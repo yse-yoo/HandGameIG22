5dab9ef1385d04dec448925f4f2863c2524e0aa9
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-05-23 10:59:51 +0900

    add Hand members

--- src/game/Hand.java
+++ src/game/Hand.java
@@ -2,4 +2,9 @@ package game;
 
 public class Hand {
 
+	final static String ROCK = "グー";
+	final static String SICCORS = "チョキ";
+	final static String PAPER = "パー";
+	final static String[] hands = { ROCK, SICCORS, PAPER };
+
 }
