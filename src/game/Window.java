ec13c913093073ca649e499fe8c16402cbfc188b
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-05-23 09:56:29 +0900

    add create

--- src/game/Window.java
+++ src/game/Window.java
@@ -6,7 +6,13 @@ public class Window extends JFrame {
 
 	//pub と入力し、Ctrl + スペース
 	public void create() {
+		//フレームのサイズ
+		setBounds(300, 200, 300, 200);
+		//フレームを閉じたときの処理
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		//フレームを表示
+		setVisible(true);
 	}
 	
 	
